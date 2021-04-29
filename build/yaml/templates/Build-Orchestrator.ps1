fd4b9a7887be3f64709dd3e106b0b432b6bdf9df
Gabo Gilabert <gabog@users.noreply.github.com>
2021-04-29 15:56:36 -0400

    Adds powershell to train and build orchestrator (#3)
    * Updated bot with the latest from composer
    Update pre-reqs to install bf cli next.
    
    * Changed the code to use orchestrator
    Deleted scripts folder
    Added powershell scripts
    
    * Added source dir
    
    * Testing download models
    
    * Updated parameter
    
    * Finished with training and renames
    
    * Changed working dir
    
    * re added qna make and lUIS
    
    * Cleanup
    
    * Removed destModelFolder parameter
    Added comments
    
    * Added parameter type
    
    * Updated param count check
    
    * Testing RC
    
    * Testing QnA with next
    
    * Updated comments cleaned up a bit
    
    * Updated text

---
+++ build/yaml/templates/Build-Orchestrator.ps1
@@ -0,0 +1,115 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# 
+# Builds and trains orchestrator models. Used in conjunction with CICD, performs the following:
+#  - Determines what models to build based on the recognizer configured for each dialog
+#  - Downloads base model(s) - English, Multilingual depending on configuration
+#  - Builds Orchestrator language models (english and multilingual) snapshot files
+#  - Creates configuration file used by runtime (orchestrator.settings.json)
+
+Param(
+	[string] $sourceDirectory,
+	[string] $crossTrainedLUDirectory,
+	[string] $appSettingsFile,
+	[string] $generatedDirectory
+)
+
+# Import script with common functions
+. ($PSScriptRoot + "/LUUtils.ps1")
+
+if ($PSBoundParameters.Keys.Count -lt 4) {
+    Write-Host "Dowloads models and trains orchestrator" 
+    Write-Host "Usage:"
+    Write-Host "`t Build-Orchestrator.ps1 -sourceDirectory ./ -crossTrainedLUDirectory ./generated/interruption -appSettingsFile ./settings/appsettings.json -generatedDirectory ./generated"  
+    Write-Host "Parameters: "
+    Write-Host "`t  sourceDirectory - Directory containing bot's source code."
+    Write-Host "`t  crossTrainedLUDirectory - Directory containing .lu/.qna files to process."
+    Write-Host "`t  appSettingsFile - Bot appsettings.json file."
+    Write-Host "`t  generatedDirectory - Directory for processed .blu files"
+    exit 1
+}
+
+# Find the lu models for the dialogs configured to use orchestrator
+$models = Get-LUModels -recognizerType "Microsoft.OrchestratorRecognizer" -crossTrainedLUDirectory $crossTrainedLUDirectory -sourceDirectory $sourceDirectory
+if ($models.Count -eq 0)
+{
+    Write-Host "No orchestrator models found."
+    exit 0        
+}
+Write-Host "Orchestrator models"
+foreach($model in $models)
+{
+    Write-Host "`t $model"
+}
+
+#Write-Host "LUIS models"
+#$models = Get-LUModels -recognizerType "Microsoft.LuisRecognizer" -crossTrainedLUDirectory $crossTrainedLUDirectory -sourceDirectory $sourceDirectory
+#foreach($model in Get-LUModels "Microsoft.LuisRecognizer" $crossTrainedLUDirectory $sourceDirectory)
+#{
+#    Write-Host $model
+#}
+
+# Load appsettings.json
+$appSettings = Get-Content -Path $appSettingsFile | ConvertFrom-Json
+
+# Determine which models we need to download and train.
+$useEnglishModel = $false
+$useMultilingualModel = $false
+Write-Output 'Loading appsettings...'
+foreach ($language in $appSettings.languages) {
+    if ($language.StartsWith('en')) {
+        $useEnglishModel = $true
+        Write-Output "`t Found English."
+    }
+    else {
+        $useMultilingualModel = $true
+        Write-Output "`t Found multilingual."
+    }
+}
+
+# Create empty Composer config file for orchestrator settings.
+$orchestratorConfig = "{
+    orchestrator:{
+        models:{},
+        snapshots:{}
+    }
+}" | ConvertFrom-Json
+
+
+# Download English model and build snapshots
+if ($useEnglishModel) 
+{
+    # Download model and update config
+    $modelDirectory = Get-OrchestratoModel -language "english" -modelDirectory "$generatedDirectory/orchestratorModels"
+    $orchestratorConfig.orchestrator.models | Add-Member -NotePropertyName en -NotePropertyValue (Get-NormalizedPath -path "$modelDirectory")
+
+    # Build snapshots
+    Build-OrchestratorSnapshots -models $models -language "english" -modelDirectory $modelDirectory -outDirectory "$generatedDirectory" -luFilesDirectory $crossTrainedLUDirectory
+}
+
+# Download multilanguage model and build snapshots
+if ($useMultilingualModel) 
+{
+    # Download model and update config
+    $modelDirectory = Get-OrchestratoModel -language "multilingual" -modelDirectory "$generatedDirectory/orchestratorModels"
+    $orchestratorConfig.orchestrator.models | Add-Member -NotePropertyName multilang -NotePropertyValue (Get-NormalizedPath -path "$modelDirectory")
+
+    # Build snapshots and update config
+    Build-OrchestratorSnapshots -models $models -language "multilingual" -modelDirectory $modelDirectory -outDirectory "$generatedDirectory" -luFilesDirectory $crossTrainedLUDirectory
+}
+
+# Update and write config file
+Write-Output "Overwriting output file $generatedDirectory/orchestrator.settings.json"
+$bluFiles = Get-ChildItem -Path "$generatedDirectory" -Include *.blu -Name
+foreach ($bluFile in $bluFiles) 
+{
+    # Update the key name so composer can recognize (remove the extension, replace . and - by _)
+    $key = $bluFile -replace ".{4}$"
+    $key = $key.Replace(".", "_")
+    $key = $key.Replace("-", "_")
+    $orchestratorConfig.orchestrator.snapshots | Add-Member -NotePropertyName "$key" -NotePropertyValue (Get-NormalizedPath -path "$generatedDirectory/$bluFile")
+}
+$orchestratorConfig | ConvertTo-Json | Out-File -FilePath "$generatedDirectory/orchestrator.settings.json"
+
+# Output the generated settings
+Get-Content "$generatedDirectory/orchestrator.settings.json"
---
+++ build/yaml/templates/LUUtils.ps1
@@ -0,0 +1,122 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# 
+# Contains a collection of common language understanding functions used in other scripts.
+#
+
+# Gets the list of lu models for a given recognizer
+function Get-LUModels
+{
+    param 
+    (
+        [string] $recognizerType,
+        [string] $crossTrainedLUDirectory,
+        [string] $sourceDirectory
+    )
+
+    # Get a list of the cross trained lu models to process
+    $crossTrainedLUModels = Get-ChildItem -Path $crossTrainedLUDirectory -Filter "*.lu" -file -name
+
+    # Get a list of all the dialog recognizers (exclude bin and obj just in case)
+    $luRecognizerDialogs = Get-ChildItem -Path $sourceDirectory -Filter "*??-??.lu.dialog" -file -name -Recurse | Where-Object { $_ -notmatch '^bin*|^obj*' }
+
+    # Add the models that matche the type to the recognizers to a list
+    $luModels = @()
+    foreach($luModel in $crossTrainedLUModels) {
+        # Load the dialog JSON and find the recognizer kind
+        $luDialog = $luRecognizerDialogs | Where-Object { $_ -match "$luModel.dialog" }
+        $dialog = Get-Content -Path "$sourceDirectory/$luDialog" | ConvertFrom-Json
+        $recognizerKind = ($dialog | Select -ExpandProperty "`$kind")
+
+        # Add it to the list if it is the expected type
+        if ( $recognizerKind -eq $recognizerType)
+        {
+            $luModels += "$crossTrainedLUDirectory/$luModel"
+        }
+    }
+
+    # return the models found
+    return $luModels
+}
+
+# Downloads the orchestrator models based on the languages configured in appsettings.json
+function Get-OrchestratoModel
+{
+    param 
+    (
+        [string] $language,
+        [string] $modelDirectory
+    )
+    
+    # Clean and recreate the model directory
+    $outDirectory = "$modelDirectory/$language"
+    if ((Test-Path -Path "$outDirectory") -eq $true) 
+    {
+        Remove-Item -Path "$outDirectory" -Force -Recurse | Out-Null
+    }
+    Write-Host "Creating $outDirectory folder..."
+    New-Item -Path "$outDirectory" -ItemType "directory" -Force | Out-Null
+    Write-Host "done."
+
+    # We only support english and multilingual for now
+    if ($language -eq "english")
+    {
+        bf orchestrator:basemodel:get -o "$outDirectory" | Out-Null
+    }
+    else
+    {
+        bf orchestrator:basemodel:get -o "$outDirectory" --versionId pretrained.20210205.microsoft.dte.00.06.unicoder_multilingual.onnx | Out-Null
+    }
+
+    return $outDirectory
+}
+
+# Builds and trains orchestrator snapshots (blu files)
+function Build-OrchestratorSnapshots
+{
+    param 
+    (
+        [string[]] $models,
+        [string] $language,
+        [string] $modelDirectory,
+        [string] $outDirectory,
+        [string] $luFilesDirectory
+
+    )
+
+    $luModels = @()
+    if ($language -eq "english")
+    {
+        $luModels = Get-ChildItem $models -Include "*en*.lu" -Name
+    }
+    else
+    {
+        $luModels = Get-ChildItem $models -Exclude "*en*.lu" -Name
+    }
+
+    # Create folder to store input lu files
+    $inputDirectory = "$modelDirectory/input"
+    if ((Test-Path -Path "$inputDirectory") -eq $false) 
+    {
+        New-Item -Path "$inputDirectory" -ItemType "directory" -Force | Out-Null
+    }
+
+    # Copy cross trained lu files to input directory
+    foreach ($luModel in $luModels)
+    {
+        Copy-Item -Path "$luFilesDirectory/$luModel" -Destination "$inputDirectory/$luModel" -Force | Out-Null
+    }
+
+    # Train
+    bf orchestrator:build --in "$inputDirectory" --out "$outDirectory" --model "$modelDirectory"
+}
+
+# Helper to replace \ by / so it works on linux and windows
+function Get-NormalizedPath
+{
+    param 
+    (
+        [string] $path
+    )
+    return $path.Replace("\", "/")
+}
