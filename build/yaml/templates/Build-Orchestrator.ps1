36d27e3fe61ab68e1f527184784a65793fac200b
Gabo Gilabert <gabog@users.noreply.github.com>
2021-04-30 13:04:25 -0400

    Updated comments and standardized parameter names and order (#5)
    * Updated comments and standarized parameter names and order
    
    * Fixed variable usage

--- build/yaml/templates/Build-Orchestrator.ps1
+++ build/yaml/templates/Build-Orchestrator.ps1
@@ -8,10 +8,10 @@
 #  - Creates configuration file used by runtime (orchestrator.settings.json)
 
 Param(
+	[string] $outputDirectory,
 	[string] $sourceDirectory,
 	[string] $crossTrainedLUDirectory,
-	[string] $appSettingsFile,
-	[string] $generatedDirectory
+	[string] $appSettingsFile
 )
 
 # Import script with common functions
@@ -20,16 +20,16 @@ Param(
 if ($PSBoundParameters.Keys.Count -lt 4) {
     Write-Host "Dowloads models and trains orchestrator" 
     Write-Host "Usage:"
-    Write-Host "`t Build-Orchestrator.ps1 -sourceDirectory ./ -crossTrainedLUDirectory ./generated/interruption -appSettingsFile ./settings/appsettings.json -generatedDirectory ./generated"  
+    Write-Host "`t Build-Orchestrator.ps1 -outputDirectory ./generated -sourceDirectory ./ -crossTrainedLUDirectory ./generated/interruption -appSettingsFile ./settings/appsettings.json"  
     Write-Host "Parameters: "
+    Write-Host "`t  outputDirectory - Directory for processed .blu files"
     Write-Host "`t  sourceDirectory - Directory containing bot's source code."
     Write-Host "`t  crossTrainedLUDirectory - Directory containing .lu/.qna files to process."
     Write-Host "`t  appSettingsFile - Bot appsettings.json file."
-    Write-Host "`t  generatedDirectory - Directory for processed .blu files"
     exit 1
 }
 
-# Find the lu models for the dialogs configured to use orchestrator
+# Find the lu models for the dialogs configured to use an Orchestrator recognizer
 $models = Get-LUModels -recognizerType "Microsoft.OrchestratorRecognizer" -crossTrainedLUDirectory $crossTrainedLUDirectory -sourceDirectory $sourceDirectory
 if ($models.Count -eq 0)
 {
@@ -73,7 +73,7 @@ $orchestratorConfig = "{
 if ($useEnglishModel) 
 {
     # Download model and update config
-    $modelDirectory = Get-OrchestratoModel -language "english" -modelDirectory "$generatedDirectory/orchestratorModels"
+    $modelDirectory = Get-OrchestratoModel -language "english" -modelDirectory "$outputDirectory/orchestratorModels"
     $orchestratorConfig.orchestrator.models | Add-Member -NotePropertyName en -NotePropertyValue (Get-NormalizedPath -path "$modelDirectory")
 
     # Create luConfig file with a list of English orchestrator models
@@ -82,14 +82,14 @@ if ($useEnglishModel)
     New-LuConfigFile -luConfig $luConfigFile -luModels ($models | Where-Object {$_ -like '*en*.lu'}) -path "$crossTrainedLUDirectory/"
 
     # Build snapshots
-    bf orchestrator:build --out "$generatedDirectory" --model "$modelDirectory" --luconfig $luConfigFile
+    bf orchestrator:build --out "$outputDirectory" --model "$modelDirectory" --luconfig $luConfigFile
 }
 
 # Download multilanguage model and build snapshots
 if ($useMultilingualModel) 
 {
     # Download model and update config
-    $modelDirectory = Get-OrchestratoModel -language "multilingual" -modelDirectory "$generatedDirectory/orchestratorModels"
+    $modelDirectory = Get-OrchestratoModel -language "multilingual" -modelDirectory "$outputDirectory/orchestratorModels"
     $orchestratorConfig.orchestrator.models | Add-Member -NotePropertyName multilang -NotePropertyValue (Get-NormalizedPath -path "$modelDirectory")
 
     # Create luConfig file with a list of Multilingual orchestrator models
@@ -98,21 +98,21 @@ if ($useMultilingualModel)
     New-LuConfigFile -luConfig $luConfigFile -luModels ($models | Where-Object {$_ -notlike '*en*.lu'}) -path "$crossTrainedLUDirectory/"
 
     # Build snapshots
-    bf orchestrator:build --out "$generatedDirectory" --model "$modelDirectory" --luconfig $luConfigFile
+    bf orchestrator:build --out "$outputDirectory" --model "$modelDirectory" --luconfig $luConfigFile
 }
 
 # Update and write config file
-Write-Output "Overwriting output file $generatedDirectory/orchestrator.settings.json"
-$bluFiles = Get-ChildItem -Path "$generatedDirectory" -Include *.blu -Name
+Write-Output "Overwriting output file $outputDirectory/orchestrator.settings.json"
+$bluFiles = Get-ChildItem -Path "$outputDirectory" -Include *.blu -Name
 foreach ($bluFile in $bluFiles) 
 {
     # Update the key name so composer can recognize (remove the extension, replace . and - by _)
     $key = $bluFile -replace ".{4}$"
     $key = $key.Replace(".", "_")
     $key = $key.Replace("-", "_")
-    $orchestratorConfig.orchestrator.snapshots | Add-Member -NotePropertyName "$key" -NotePropertyValue (Get-NormalizedPath -path "$generatedDirectory/$bluFile")
+    $orchestratorConfig.orchestrator.snapshots | Add-Member -NotePropertyName "$key" -NotePropertyValue (Get-NormalizedPath -path "$outputDirectory/$bluFile")
 }
-$orchestratorConfig | ConvertTo-Json | Out-File -FilePath "$generatedDirectory/orchestrator.settings.json"
+$orchestratorConfig | ConvertTo-Json | Out-File -FilePath "$outputDirectory/orchestrator.settings.json"
 
 # Output the generated settings
-Get-Content "$generatedDirectory/orchestrator.settings.json"
+Get-Content "$outputDirectory/orchestrator.settings.json"
